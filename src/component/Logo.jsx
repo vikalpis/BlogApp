1d0e10c1deceadb191b6afe5d2d8526f0db1c53a
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-02-03 20:02:08 +0530

    feat: Update Logo component to use an image and change favicon and title in index.html

--- src/component/Logo.jsx
+++ src/component/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo( width = "100px") {
   return (
-    <div>Logo</div>
+    <img src="Logo.png" alt="logo" className='h-12 w-auto max-w-[140px] object-contain md:h-14 lg:h-16 rounded-3xl'/>
   )
 }
 
